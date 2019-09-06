c7ef0f605f81025504de233849234dbf375ea1ab
admin <admin@admin-PC>
2019-09-06 22:11:49 +0530

    First commit

---
+++ AdidasEcomm/src/main/java/AdidasEcomm/AdidasEcomm/App.java
@@ -0,0 +1,13 @@
+package AdidasEcomm.AdidasEcomm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
